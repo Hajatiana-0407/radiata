cc5800d415635e75fbc10d1eb05a282683ad48f0
Hajatiana-0407 <hajatianarasolondraibe0407@gmail.com>
2025-12-13 11:31:47 +0300

    Remove Client/.next from git tracking

--- Client/.next/dev/static/chunks/app_about_page_tsx_4815ba62._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_b1f22faa._.js",
-  "static/chunks/node_modules_48bb746e._.js"
-],
-    source: "dynamic"
-});
