acf3d274875d3cb74ad3ebc801f75c7fdb7a94f8
Hajatiana-0407 <hajatianarasolondraibe0407@gmail.com>
2026-01-21 03:56:05 +0300

    fix ( Api ) : change the api url for production mode

--- Client/lib/api/client.ts
+++ Client/lib/api/client.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
 
 export const apiClient = axios.create({
-  baseURL: API_BASE_URL + '/api',
+  baseURL: API_BASE_URL + '/index.php/api',
   headers: {
     'Content-Type': 'application/json',
   },
